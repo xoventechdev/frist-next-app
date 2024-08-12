3ab5820fd572f7a7e6274cf94c73c7fc01f85958
xoventechdev <alkarim.doha@gmail.com>
2024-08-12 18:22:56 +0300

    Route Ongoing

---
+++ src/app/book/add/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import css from "../../page.module.css";
+import addCss from "./bookadd.module.css";
+
+const page = () => {
+  return (
+    <div className={css.description}>
+      <h2 className={addCss.header}>Add Book</h2>
+    </div>
+  );
+};
+
+export default page;
